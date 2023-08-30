dc8d8c818b9f9ccecbe893851600cee44da5b09c
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-08-30 22:31:42 +0100

    Initial Commit

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["src"],
+	target: "esnext",
+	format: ["esm", "cjs"],
+	splitting: false,
+	sourcemap: true,
+	clean: true,
+});
